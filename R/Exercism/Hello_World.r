a262da1d809179c28b5d83345b2aa54ae27c80ec
Yurim Yang <102911117+yulimy0113@users.noreply.github.com>
2024-11-21 23:50:57 +0900

    Update and rename Hello World.r to Hello_World.r

---
+++ R/Exercism/Hello_World.r
@@ -0,0 +1,6 @@
+# “Hello, World!” will get you writing some R and familiarize yourself with the Exercism workflow. 
+# Completing it unlocks the rest of the R
+
+hello_world <- function() {
+    "Hello, World!"
+}
