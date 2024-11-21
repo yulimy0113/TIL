a03fe0b5a2791adc329463ad59af540b70ece307
Yurim Yang <102911117+yulimy0113@users.noreply.github.com>
2024-11-21 23:51:41 +0900

    Create Two_Fer.r

---
+++ R/Exercism/Two_Fer.r
@@ -0,0 +1,14 @@
+# Your task is to determine what you will say as you give away the extra cookie.
+# If you know the person's name (e.g. if they're named Do-yun), 
+# then you will say: "One for Do-yun, one for me."
+# If you don't know the person's name, 
+# you will say you instead. "One for you, one for me."
+
+two_fer <- function(input=FALSE) {
+  if (input == FALSE){
+    return("One for you, one for me.")
+  }
+  else{
+    return(paste("One for ", input, ", one for me.", sep = ""))
+  }
+}
